484ece851a0d3c9bfd8b97ebe707f8f081e6fc5b
Anuja Dilrukshi Herath <anuanujadilrukshi@gmail.com>
2021-07-15 12:29:19 +0530

    This is the first commit

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String [] args){
+        int value=20;
+        short shot = 93;
+
+        System.out.println(value);
+        System.out.println(shot);
+    }
+}
